f311f7d1a75ef1a2748264ad18861e803af31e5c
MADITIS <97931292+MADITIS@users.noreply.github.com>
2023-03-25 13:31:56 +0530

    Update settings.py

--- sorter/settings.py
+++ sorter/settings.py
@@ -2,13 +2,13 @@ class Config:
     '''
     Necessary Config settings.
     '''
-    TG_USER_ID: str = 'f'
-    AUTHORIZED_GROUPS: list[str] = ['test']
+    TG_USER_ID: str = ''
+    AUTHORIZED_GROUPS: list[str] = ['authourized groups']
 
-    DRIVE_SOURCE_DRIVE_ID: str = '0AIyUCwBELSPnUk9PVA'
-    DRIVE_SOURCE_FOLDER_ID: str = '1alZiCB8v8_9cs3Fttx-FS2G8fDe4_3Kv'
+    DRIVE_SOURCE_DRIVE_ID: str = ''
+    DRIVE_SOURCE_FOLDER_ID: str = ''
 
-    DRIVE_DESTINATION_DRIVE_ID: str = '0AIyUCwBELSPnUk9PVA'
-    DRIVE_DESTINATION_FOLDER_ID: str = '1q7WmyErsI3FqxsMsPuNCm6mPzyoMhRjz'
+    DRIVE_DESTINATION_DRIVE_ID: str = ''
+    DRIVE_DESTINATION_FOLDER_ID: str = ''
 
 config = Config()
